a7b7aa2461c3d904b418f9534a1c391d21a7fc79
Ben Stidham <ben.stidham1@gmail.com>
2023-11-04 15:32:21 -0500

    Modified some stuff and changes some files

--- app/src/main/java/com/hackutdx/MainActivity.java
+++ app/src/main/java/com/hackutdx/MainActivity.java
@@ -3,6 +3,14 @@ package com.hackutdx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.ar.core.*;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+
+import java.util.EnumSet;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void createSession() throws UnavailableDeviceNotCompatibleException, UnavailableSdkTooOldException, UnavailableArcoreNotInstalledException, UnavailableApkTooOldException {
+        Session session = new Session(this);
+        Config config = new Config(session);
+        session.configure(config);
+        CameraConfigFilter filter = new CameraConfigFilter(session);
+        filter.setTargetFps(EnumSet.of(CameraConfig.TargetFps.TARGET_FPS_30));
+        filter.setDepthSensorUsage(EnumSet.of(CameraConfig.DepthSensorUsage.DO_NOT_USE));
+        List<CameraConfig> cameraConfigList = session.getSupportedCameraConfigs(filter);
+        session.setCameraConfig(cameraConfigList.get(0));
+
     }
 }
